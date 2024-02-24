210335e6932f1e472deb6a74d2204d12ef34287e
Catherine Xie <catherinexie2014@gmail.com>
2024-02-24 00:38:13 -0500

    Setting up globe visualization

--- client/src/GlobeVisualization.js
+++ client/src/GlobeVisualization.js
@@ -1,106 +1,274 @@
-import React, { useEffect, useRef } from 'react'
+// import React, {
+//   useEffect,
+//   useRef,
+//   onClick,
+//   onWindowResize,
+//   onMouseMove,
+// } from 'react'
+// import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+// import * as THREE from 'three'
+// import ThreeGlobe from 'three-globe'
+// import countries from './custom.geo.json'
+
+import React, { useEffect, useRef, useState } from 'react'
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
 import * as THREE from 'three'
 import ThreeGlobe from 'three-globe'
 import countries from './custom.geo.json'
 
-const GlobeVisualization = () => {
+function GlobeVisualization() {
   const globeContainerRef = useRef()
+  const [mouseX, setMouseX] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
+  const windowHalfX = window.innerWidth / 2
+  const windowHalfY = window.innerHeight / 2
+  let renderer, camera, scene, controls
+  let Globe
 
   useEffect(() => {
-    // Gen random data
-    // const N = 300
-    // const gData = [...Array(N).keys()].map(() => ({
-    //   lat: (Math.random() - 0.5) * 180,
-    //   lng: (Math.random() - 0.5) * 360,
-    //   size: Math.random() / 3,
-    //   color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
-    // }));
-
-        
-    // Setup renderer
-    const renderer = new THREE.WebGLRenderer({antialias:true})
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    globeContainerRef.current.appendChild(renderer.domElement)
-
-
-    // Setup scene
-    const scene = new THREE.Scene()
-    
-    scene.add(new THREE.AmbientLight(0xbbbbbb, 0.3))
-    scene.add(new THREE.DirectionalLight(0xffffff, 0.6 * Math.PI))
-
-    // Setup camera
-    const camera = new THREE.PerspectiveCamera()
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    camera.position.z = 400
-    // camera.add()
-    scene.add(camera)
-
-    // Add camera controls
-    const tbControls = new TrackballControls(camera, renderer.domElement)
-    tbControls.minDistance = 101
-    tbControls.rotateSpeed = 5
-    tbControls.zoomSpeed = 0.8
-
-
-    const Globe = new ThreeGlobe({
-        waitForGlobeReady: true,
-        animateIn:true,
-    })
-      .hexPolygonsData(countries.features)
-      .hexPolygonResolution(3)
-      .hexPolygonMargin(0.7)
-      .showAtmosphere(true)
-      .atmosphereColor('#3a228a')
-      .atmosphereAltitude(0.25)
-
-    //   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-    //   .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-    //   .pointsData(gData)
-    //   .pointAltitude('size')
-    //   .pointColor('color');
-
-    // setTimeout(() => {
-    //   gData.forEach((d) => (d.size = Math.random()));
-    //   Globe.pointsData(gData);
-    // }, 4000);
-
-    const globeMaterial = Globe.globeMaterial()
-    globeMaterial.color = new THREE.Color(0x3a228a)
-    scene.add(Globe)
-
-    // Kick-off renderer
-    const animate = () => {
-      // Frame cycle
-      tbControls.update()
-      renderer.render(scene, camera)
-      requestAnimationFrame(animate)
-    }
-
-    animate()
-
-    // Clean up
-    return () => {
-    //   Globe.dispose()
-      renderer.dispose()
-      tbControls.dispose()
-    }
+    init()
+    initGlobe()
+    // animate();
+    // onWindowResize();
   }, [])
 
-  return (
-    <div>
-      {/* <p> boo</p> */}
-      <div ref={globeContainerRef}/>
-      {/* <p> i done</p> */}
-    </div>
-  )
+  function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    scene = new THREE.Scene()
+
+    const ambientLight = new THREE.AmbientLight(0xbbbbbb, 1)
+    scene.add(ambientLight)
+    scene.background = new THREE.Color(0x040d21)
+
+    camera = new THREE.PerspectiveCamera()
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+    camera.position.z = 400
+    camera.position.x = 0
+    camera.position.y = 0
+
+    scene.add(camera)
+
+    scene.fog = new THREE.Fog(0x535ef3, 400, 2000)
+
+    window.addEventListener('resize', onWindowResize, false)
+    document.addEventListener('mousemove', onMouseMove)
+  }
+
+  function initGlobe() {
+    Globe = new ThreeGlobe({
+      waitForGlobeReady: true,
+      animateIn: true,
+    })
+    const globeMaterial = Globe.globeMaterial()
+    globeMaterial.color = new THREE.Color(0x3a228a)
+    globeMaterial.emissive = new THREE.Color(0x220038)
+    globeMaterial.emissiveIntensity = 0.1
+    globeMaterial.shininess = 0.7
+
+    scene.add(Globe)
+  }
+
+  function onMouseMove(event) {
+    mouseX = event.clientX - windowHalfX
+    mouseY = event.clientY - windowHalfY
+  }
+
+  function onWindowResize() {
+    // resize code here
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    windowHalfX = window.innerWidth / 1.5
+    windowHalfX = window.innerHeight / 1.5
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  function animate() {
+    camera.position.x +=
+      Math.abs(mouseX) <= windowHalfX / 2
+        ? (mouseX / 2 - camera.position.x) * 0.005
+        : 0;
+    camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005
+    camera.lookAt(scene.position)
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+  }
+
+  return <div ref={globeContainerRef}>hi there</div>
 }
 
 export default GlobeVisualization
 
+// var renderer, camera, scene, controls
+
+// let mouseX = 0
+// let mouseY = 0
+// let windowHalfX = window.innerWidth / 2
+// let windowHalfY = window.innerHeight / 2
+// var Globe
+
+// init()
+// initGlobe()
+// // onWindowResize()
+// // animate()
+
+// function init() {
+//   renderer = new THREE.WebGL1Renderer({ antialias: true })
+//   renderer.setPixelRatio(window.devicePixelRatio)
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+//   document.body.appendChild(renderer.domElement)
+
+//   scene = new THREE.Scene()
+
+//   var AmbientLight = new THREE.AmbientLight(0xbbbbbb, 1)
+//   scene.add(AmbientLight)
+//   scene.background = new THREE.Color(0x040d21)
+
+//   camera = new THREE.PerspectiveCamera()
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+
+//   var DirectionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+//   // DirectionalLight.getWorldPosition
+
+//   camera.position.z = 400
+//   camera.position.x = 0
+//   camera.position.y = 0
+
+//   scene.add(camera)
+
+//   scene.fog = new THREE.Fog(0x535ef3, 400, 2000)
+
+//   // controls = new OrbitCon
+
+//   window.addEventListener('resize', onWindowResize, false)
+//   document.addEventListener('mousemove', onMouseMove)
+// }
+
+// function initGlobe() {
+//   Globe = new ThreeGlobe({
+//     waitForGlobeReady: true,
+//     animateIn: true,
+//   })
+// }
+// const GlobeVisualization = () => {
+//   const globeContainerRef = useRef()
+
+//   useEffect(() => {
+//     // Gen random data
+//     const N = 1
+//     const gData = [...Array(N).keys()].map(() => ({
+//       lat: 43.6532, //(Math.random() - 0.5) * 180,
+//       lng: -79.3832, //(Math.random() - 0.5) * 360,
+//       size: 1, //Math.random() / 3,
+//       color: 'white', //['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
+//     }))
+
+//     const Globe = new ThreeGlobe({
+//       waitForGlobeReady: true,
+//       animateIn: true,
+//     })
+//       .hexPolygonsData(countries.features)
+//       .hexPolygonResolution(3)
+//       .hexPolygonMargin(0.7)
+//       .showAtmosphere(true)
+//       .atmosphereColor('#3a228a')
+//       .atmosphereAltitude(0.25)
+
+//       // .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+//       // .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+//       .pointsData(gData)
+//       .pointAltitude('size')
+//       .pointColor('color')
+//       .labelsData(gData)
+//       .labelText(d => `TORONTO`) // (${Math.round(d.lat * 1e2) / 1e2}, ${Math.round(d.lng * 1e2) / 1e2})`)
+//       .labelSize('size')
+//       // .labelDotRadius(d => d.size / 5)
+//       .labelColor('color');
+
+//     // Setup renderer
+//     const renderer = new THREE.WebGLRenderer() //({antialias:true})
+//     // renderer.setPixelRatio(window.devicePixelRatio)
+//     renderer.setSize(window.innerWidth, window.innerHeight)
+//     globeContainerRef.current.appendChild(renderer.domElement)
+//     // document.getElementById('globe').appendChild(renderer.domElement) // ADDED
+
+//     // Setup scene
+//     const scene = new THREE.Scene()
+
+//     scene.add(new THREE.AmbientLight(0xbbbbbb, 1)) //0.3))
+//     // scene.add(new THREE.DirectionalLight(0xffffff, 0.6 * Math.PI))
+
+//     // Setup camera
+//     const camera = new THREE.PerspectiveCamera()
+//     camera.aspect = window.innerWidth / window.innerHeight
+//     camera.updateProjectionMatrix()
+//     camera.position.z = 400
+//     // camera.add()
+//     scene.add(camera)
+
+//     // Add camera controls
+//     const tbControls = new TrackballControls(camera, renderer.domElement)
+//     tbControls.minDistance = 101
+//     tbControls.rotateSpeed = 5
+//     tbControls.zoomSpeed = 0.8
+
+//     setTimeout(() => {
+//       gData.forEach((d) => (d.size = 1 / 4)) //Math.random()));
+//       Globe.pointsData(gData)
+//     }, 4000)
+
+//     const globeMaterial = Globe.globeMaterial()
+//     globeMaterial.color = new THREE.Color(0x3a228a)
+//     scene.add(Globe)
+
+//     // scene.traverse((object) => {
+//     //   if (object instanceof THREE.Mesh) {
+//     //     object.castShadow = false;
+//     //     object.receiveShadow = false;
+//     //   }
+//     // });
+
+//     // Kick-off renderer
+//     const animate = () => {
+//       // Frame cycle
+//       tbControls.update()
+//       renderer.render(scene, camera)
+//       requestAnimationFrame(animate)
+//     }
+
+//     animate()
+
+//     // renderer.domElement.addEventListener('click', onClick)
+
+//     // Clean up
+//     return () => {
+//       // renderer.domElement.removeEventListener('click', onClick);
+//       renderer.dispose()
+//       tbControls.dispose()
+//     }
+//   }, [])
+
+//   const onClick = (event) => {
+//     console.log(`HI THERE`)
+//   }
+
+//   return (
+//     <div>
+//       {/* <p> boo</p> */}
+//       <div ref={globeContainerRef} onClick={onClick} />
+//       {/* <p> i done</p> */}
+//     </div>
+//   )
+// }
+
+// export default GlobeVisualization
 
 // import React, { useEffect, useRef } from 'react';
 // import * as THREE from 'three';
