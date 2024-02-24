497c5b69021cb7f9a7fe814a8910a06bb701095b
Catherine Xie <catherinexie2014@gmail.com>
2024-02-24 13:20:32 -0500

    Layout and elements of searchbar

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    <div className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 absolute">
-      <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+    <div className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 absolute w-1/3">
+      <label htmlFor="city" className="block text-sm font-medium text-gray-500">
         City Name
       </label>
       <input
@@ -11,46 +11,51 @@ function SearchBar() {
         id="city"
         className="mt-1 p-2 block w-full border border-gray-300 rounded-md bg-white dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       />
-
-      <div className="mt-4">
-        <label
-          htmlFor="longitude"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Longitude
-        </label>
-        <input
-          type="text"
-          id="longitude"
-          className="mt-1 p-2 block w-full border border-gray-300 rounded-md bg-white dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+      <div className="mt-4 flex justify-center">
+        <p className="block text-sm font-medium text-gray-100">OR</p>
       </div>
-
-      <div className="mt-4">
-        <label
-          htmlFor="latitude"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Latitude
-        </label>
-        <input
-          type="text"
-          id="latitude"
-          className="mt-1 p-2 block w-full border border-gray-300 rounded-md bg-white dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+      <div className="flex justify-between space-x-5">
+        <div className="mt-4">
+          <label
+            htmlFor="longitude"
+            className="block text-sm font-medium text-gray-500"
+          >
+            Longitude
+          </label>
+          <input
+            type="text"
+            id="longitude"
+            className="mt-1 p-2 block w-full border border-gray-300 rounded-md bg-white dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div className="mt-4">
+          <label
+            htmlFor="latitude"
+            className="block text-sm font-medium text-gray-500"
+          >
+            Latitude
+          </label>
+          <input
+            type="text"
+            id="latitude"
+            className="mt-1 p-2 block w-full border border-gray-300 rounded-md bg-white dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
       </div>
-      <button
-        type="button"
-        className="mt-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Clear
-      </button>
-      <button
-        type="button"
-        className="mt-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Search
-      </button>
+      <div className="mt-4 flex justify-between">
+        <button
+          type="button"
+          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Clear
+        </button>
+        <button
+          type="button"
+          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Search
+        </button>
+      </div>
     </div>
   )
 }
